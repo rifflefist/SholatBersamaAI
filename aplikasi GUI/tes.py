7a2daee52da477269d2b62ab23e2800762800998
raihanalifyalubis <raihan.alifya22@gmail.com>
2025-06-26 02:13:24 +0700

    Add: Animation hover button

--- aplikasi GUI/tes.py
+++ aplikasi GUI/tes.py
@@ -14,18 +14,20 @@ def quit_app():
 def on_enter(e):
     e.widget['background'] = '#007acc'  # Warna saat hover
     e.widget['foreground'] = 'white'
+    e.widget.config(cursor="hand2")
 
 def on_leave(e):
     e.widget['background'] = 'SystemButtonFace'  # Warna default
     e.widget['foreground'] = 'black'
+    e.widget.config(cursor="")
     
 def on_enter_play(e):
-    e.widget['background'] = "#1ca4ff"
-    e.widget['foreground'] = 'white'
+    label_play.config(image=play_img_hover)
+    e.widget.config(cursor="hand2")
 
 def on_leave_play(e):
-    e.widget['background'] = '#add8e6'
-    e.widget['foreground'] = 'black'
+    label_play.config(image=play_img)
+    e.widget.config(cursor="")
 
 window = tk.Tk()
 window.title("Sholat Bersama AI")
@@ -36,11 +38,14 @@ window.minsize(800, 600)
 img = Image.open("assets/images/play.png").resize((300, 300), Image.LANCZOS)
 play_img = ImageTk.PhotoImage(img)
 
-label = tk.Label(window, image=play_img, bg="#add8e6", borderwidth=0)
-label.place(relx=0.5, rely=0.3, anchor="center")
-label.bind("<Button-1>", start)
-label.bind("<Enter>", on_enter_play)
-label.bind("<Leave>", on_leave_play)
+img_hover = Image.open("assets/images/play_hover.png").resize((300, 300), Image.LANCZOS)
+play_img_hover = ImageTk.PhotoImage(img_hover)
+
+label_play = tk.Label(window, image=play_img, bg="#add8e6", borderwidth=0)
+label_play.place(relx=0.5, rely=0.3, anchor="center")
+label_play.bind("<Button-1>", start)
+label_play.bind("<Enter>", on_enter_play)
+label_play.bind("<Leave>", on_leave_play)
 
 # Tombol Settings (kiri bawah)
 btn_settings = tk.Button(window, text="Settings", font=("Arial", 16), command=settings)
